97d540d884620bdde49c7965797d5cafb75d9cab
Jan Prazak <janp@horasion.eu>
2022-07-18 15:12:13 +0200

    added JAR file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
+import eu.horasion.basics.oop.carfactory.CarFactoryDemo;
+
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+
+		if(args.length != 0) {
+			System.out.println("Arguments detected, listing:");
+			for(String a : args) {
+				System.out.println(a);
+			}
+		}
+
+		System.out.println("Starting the car factory demo...");
+		CarFactoryDemo.main(args);
 	}
 }
